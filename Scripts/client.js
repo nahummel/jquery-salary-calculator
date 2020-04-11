0657a6139095ca63b3aab706cb28e7407940cef7
Natalie Hummel <natoms225@gmail.com>
2020-04-11 13:23:08 -0500

    table displays inputs when submitted

--- Scripts/client.js
+++ Scripts/client.js
@@ -16,10 +16,29 @@ function getInputs() {
         title: $('#title').val(),
         annualSalary: $('#annualSalary').val(),
     }
+
     employeeList.push(employee);
+
     $('#firstName').val('');
     $('#lastName').val('');
     $('#id').val('');
     $('#title').val('');
     $('#annualSalary').val('');
+
+    displayInputs();
+}
+
+function displayInputs() {
+    $('tbody').empty();
+
+    for (const person of employeeList) {
+        const newRow = $('tbody').append(`<tr></tr>`)
+        newRow.append(`<td>${person.firstName}</td>`)
+        newRow.append(`<td>${person.lastName}</td>`)
+        newRow.append(`<td>${person.id}</td>`)
+        newRow.append(`<td>${person.title}</td>`)
+        newRow.append(`<td>${person.annualSalary}</td>`)
+    
+    } 
+
 }
